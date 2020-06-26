6f0feba4e4860ba6388243ba2ef0de2acc1afb6d
simon <simon@uwotech.com>
2020-06-27 02:54:54 +0800

    创建 session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   get 'hello', to: 'first#hello'
   get 'hi', to: 'first#hi'
 
-  #get '/users', to: 'users#index'
-  #get '/users/:id', to: 'users#show'
-  #post '/users', to: 'users#create'
-  #delete '/users/:id', to: 'users#destroy'
-  #patch '/users/:id', to: 'users#update'
-
   resources :users
+  resources :sessions, only: [:create, :destroy]
 end
